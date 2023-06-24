37a9a53ae122d8d22005b3548b17ddfae4b5cbf4
cbvfgtn217@gmail.com <cbvfgtn217@gmail.com>
2023-06-24 15:51:19 +0300

    юниты

---
+++ AdminTests/ProductsPageTests.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ComfortStoreLibrary.Models;
+using Admin.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admin.Pages.Tests
+{
+    [TestClass()]
+    public class ProductsPageTests
+    {
+        [TestMethod()]
+        public void ProductsPageTest()
+        {
+            Product prod = new Product();   
+            prod.Title = "Test";
+            prod.Description = "TestDes"; 
+            prod.Price = 100;
+            prod.Count = 2;
+            App.db.Product.Add(prod);
+            App.db.SaveChanges();
+            var product = App.db.Product.FirstOrDefault(x => x.Title == prod.Title);
+            Assert.IsTrue(product != null);
+
+
+        }
+
+        [TestMethod()]
+        public void ProductsPageTest1()
+        {
+            Product prod = new Product();
+            prod.Title = "Test1";
+            prod.Description = "TestDes";
+            prod.Price = 100;
+            prod.Count = 2;
+            App.db.Product.Add(prod);
+            App.db.SaveChanges();
+            var product = App.db.Product.FirstOrDefault(x => x.Title == prod.Title);
+            prod.Title = "123";
+            prod.Count = 0;
+            App.db.SaveChanges();
+            var product1 = App.db.Product.FirstOrDefault(x => x.Title == "123");
+            Assert.IsTrue(product != null);
+
+
+        }
+
+        [TestMethod()]
+        public void ProductsPageTest2()
+        {
+            Product prod = new Product();
+            prod.Title = "Test2";
+            prod.Description = "TestDes";
+            prod.Price = 100;
+            prod.Count = 2;
+            App.db.Product.Add(prod);
+            App.db.SaveChanges();
+
+            var product = App.db.Product.FirstOrDefault(x => x.Title == prod.Title);
+            App.db.Product.Remove(product);
+            App.db.SaveChanges();
+            Assert.IsTrue(product != null);
+
+
+        }
+    }
+}
---
+++ AdminTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью указанного ниже 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// общие сведения об этой сборке.
+[assembly: AssemblyTitle("AdminTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AdminTests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка параметра ComVisible в значение false делает типы в этой сборке невидимыми 
+// для компонентов модели COM.  Если тип в этой сборке необходимо сделать доступным 
+// модели COM, задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("8c4d0a5e-08c2-48b0-8e00-06da8051f130")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [сборка: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
